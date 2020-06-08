17bc1e166f7730b148ffd3285dc037ccae70a459
Léo SAMUEL <leosamuel64@gmail.com>
2020-06-08 19:58:04 +0200

    ajout de music

--- main.py
+++ main.py
@@ -10,20 +10,61 @@ pygame.init()
 pygame.mixer.init()
 pygame.font.init()
 
+l,h= 1280,960
 
-ecran = pygame.display.set_mode((1280,960))
+ecran = pygame.display.set_mode((l,h))
 
 #os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 jeu = True
 
 
+music_intro = pygame.mixer.Sound("data/music/intro")
+
+def texte(text,size):
+	f = pygame.font.Font(None,size)
+	textFont = f.render(text,True,(0,0,0))
+	return textFont
+
+def intro():
+	jeu = True
+
+	leo = texte("Léo", 25)
+	Lv = texte("Louis-Victor",25)
+	presente = texte("Présente : ",25)
+
+	t=0
+	t1=10/3
 
 
-ptsvent = 10
+	while jeu:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				jeu = False
+
+		if t < t1:
+			ecran.blit(leo, (l/2,h/2)) 
+		elif t<2*t1:
+			ecran.blit(Lv, (l/2,h/2)) 
+		elif t<3*t1:
+			ecran.blit(presente, (l/2,h/2)) 
+		else:
+			jeu = False
+
+		
+		# ecran.blit(fond,(0,0))
+		# ecran.blit(ballon, (x,y)) # perso
+		time.sleep(0.002)
+		pygame.display.flip()
+
+	
+	
+
+
+
 x=400
 y=1280/2
-
+"""
 while jeu:
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -44,3 +85,6 @@ while jeu:
 	# ecran.blit(ballon, (x,y)) # perso
 	time.sleep(0.002)
 	pygame.display.flip()
+"""
+
+intro()
