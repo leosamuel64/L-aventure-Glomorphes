bfa03d40c9bfef240b669b8a641cb1f9c4e66bd2
Léo SAMUEL <leosamuel64@gmail.com>
2020-06-18 12:15:52 +0200

    Ajout de la scene jeuEspace

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import time
 import random
 from pygame.locals import *
+import math
 
 """
 ------------------------------  INITIALISATION DE PYGAME  ------------------------------
@@ -44,6 +45,12 @@ def texte(text,size):
 	textFont = f.render(text,True,(255,255,255))
 	return textFont
 
+def HitBox(x,y,sizeY,sizeX):
+	""" Créer une Hitbox rectangulaire de coin gauche supérieur x,y"""
+	return 	[	(x-sizeX,y-sizeY),
+				(x+sizeX,y+sizeY)
+			]		
+
 def dansBoite(box,x,y):
 	"""
 	Entrées	:	- box : Liste de tupple (représente les coordonées du coin haut gauche et bas droit d'un rectangle)
@@ -52,7 +59,7 @@ def dansBoite(box,x,y):
 
 	Sortie  : 	- Indique si le point (x,y) est dans le rectangle représentée par box
 	"""
-	if x>box[0][0] and x < box[1][0] and  y>box[0][1] and y < box[1][1]:
+	if (x>box[0][0] and x < box[1][0]) and  (y>box[0][1] and y < box[1][1]):
 		return True
 	else:
 		return False
@@ -84,39 +91,32 @@ def image(chemin,x,y):
 ------------------------------  SCENES DU JEU  ------------------------------
 """
 
-def select_taille_ecran():
-	x,y = 100,80
+def select_taille_ecran(x,y):
 	incr = 100
 	ecran = pygame.display.set_mode((x,y))
 	t = texte("Ajustez la taille de la fenêtre avec Z-S et D-Q puis ESPACE ",int(x/9))
 	jeu = True
-
 	
-	
-	while jeu:
-		
+	while jeu:		
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				jeu = False
 			if event.type == KEYDOWN:
 				if event.key == K_ESCAPE:
 					jeu = False
+
 				if event.key == K_z:
 					y+=incr
 					ecran = pygame.display.set_mode((x,y))
-
 				if event.key == K_s and (y-incr > 0):
 					y-=incr
 					ecran = pygame.display.set_mode((x,y))
-		
 				if event.key == K_d:
 					x+=incr
 					ecran = pygame.display.set_mode((x,y))
-		
 				if event.key == K_q and  (y-incr > 0):
 					x-=incr
 					ecran = pygame.display.set_mode((x,y))
-				
 				if event.key == K_SPACE :
 					return x,y
 			
@@ -203,7 +203,8 @@ def menu():
 				## Récupère l'état des boutons sous la forme d'un tupple 0/1 (Gauche,molette,Droite)
 			press = pygame.mouse.get_pressed()
 			if press[0] == 1:
-				print("Jouer")
+				jeuEspace()
+				jeu=False
 		else:
 			ecran.blit(Jouertxt, jouerBox[0])
 
@@ -253,10 +254,7 @@ def info():
 	while jeu:
 			## On récupère la possition de la souris
 		x,y = pygame.mouse.get_pos()
-
-
 		ecran.fill((0,0,0))
-		
 			## Detection de la croix
 		for event in pygame.event.get():
 				if event.type == QUIT:
@@ -279,13 +277,36 @@ def info():
 		pygame.display.flip()
 
 def jeuEspace():
-	jeu = True
+	music("data/music/Purple.mp3")
+
 	fusée = image("data/picture/fusee.png",100,100)
-	# fuséeflamme = image("data/picture/fuseeflamme.png",100,100)
-	angle = 0
-	incr_angle = 90
-	x,y= l/2,h/2
+	fuséeflamme = image("data/picture/fuseeflamme.png",100,100)
+	fond = image("data/picture/fondEspace.jpg",l,h)
+
+	asteroide = image("data/picture/asteroide.png",100,100)
+	asteroide2 = image("data/picture/asteroide.png",100,100)
+
+	vitesse=0.3
+	PointsVie = 10
+	Distance = 30000
 	
+	x,y= l/2,h/2
+	xast1,yast1 = 100,100
+	xast2,yast2 = l-1,h-1
+	
+	momentumX=0
+	momentumY=0
+	momentumXast1,momentumYast1 = random.randint(1,3),random.randint(-3,3)
+	momentumXast2,momentumYast2 = random.randint(-3,3),random.randint(1,3)
+
+	invstate = False
+
+	txtPV = texte("Etat du fuselage : "+str(PointsVie)+" /10",30)
+	txtVitesse=texte("Vitesse de la fusée : "+str(abs(momentumX)+abs(momentumY))+" m/s",30)
+	txtDistance = texte("Distance vers l'objectif : "+str(Distance)+" m",30)
+	
+	pygame.key.set_repeat(1,20)	
+	jeu = True
 
 	while jeu:
 		for event in pygame.event.get():
@@ -294,60 +315,109 @@ def jeuEspace():
 			if event.type == KEYDOWN:
 				if event.key == K_ESCAPE:
 					jeu = False
-				if event.key == K_z:
-					print("Z")
-				if event.key == K_s:
-					print("S")
-				if event.key == K_d:
-					fusée = pygame.transform.rotate(fusée, incr_angle)
-					print("d")
-				if event.key == K_q:
-					fusée = pygame.transform.rotate(fusée, incr_angle)
-					print("q")
 
-		# fusée = pygame.transform.rotate(fusée, angle)
-		ecran.fill((0,0,0))
-		# ecran.blit(fond,(0,0))
-		ecran.blit(fusée, (x,y)) # perso
+				if event.key == K_z:
+					momentumY-=vitesse
+				if event.key == K_s:
+					momentumY+=vitesse
+				if event.key == K_d:
+					momentumX+=vitesse
+				if event.key == K_q:
+					momentumX-=vitesse
+		
+		# Test pour empecher la fusée de sortir de l'image
+		if x>l or x<0:
+			x=l/2
+			momentumX=0
+		if y>h or y<0:
+			y=h/2
+			momentumY=0
+
+		# Calcul des coords pour la fusée et les astéroides
+		x+= momentumX
+		y+=momentumY
+		xast1+= momentumXast1
+		yast1+=momentumYast1
+		xast2+= momentumXast2
+		yast2+=momentumYast2
+		
+		# Replace les astéroides
+		if xast1>l or yast1>h or xast1<0 or yast1<0:
+			rand = random.random()
+			if rand < 0.25:
+				xast1,yast1 = int(l/2),1
+				momentumXast,momentumYast = random.randint(-3,3),random.randint(1,3)
+			elif rand < 0.50:
+				xast1,yast1 = 1,int(h/2)
+				momentumXast,momentumYast = random.randint(-3,3),random.randint(1,3)
+			elif rand < 0.75:
+				xast1,yast1 = int(l/2),h-1
+				momentumXast,momentumYast = random.randint(-3,3),random.randint(-3,-1)
+			else:
+				xast1,yast1 = l-1,int(h/2)
+				momentumXast,momentumYast = random.randint(-3,-1),random.randint(-3,-1)
+
+		if xast2>l or yast2>h or xast2<0 or yast2<0:
+			rand = random.random()
+			if rand < 0.25:
+				xast2,yast2 = int(l/2),1
+				momentumXast2,momentumYast2 = random.randint(-3,3),random.randint(1,3)
+			elif rand < 0.50:
+				xast2,yast2 = 1,int(h/2)
+				momentumXast2,momentumYast2 = random.randint(-3,3),random.randint(1,3)
+			elif rand < 0.75:
+				xast2,yast2 = int(l/2),h-1
+				momentumXast2,momentumYast2 = random.randint(-3,3),random.randint(-3,-1)
+			else:
+				xast2,yast2 = l-1,int(h/2)
+				momentumXast2,momentumYast2 = random.randint(-3,-1),random.randint(-3,-1)
+		# On vérifie les collisions
+		BoxAst1 = HitBox(xast1,yast1,50,50)
+		BoxAst2 = HitBox(xast2,yast2,50,50)
+
+		if (dansBoite(BoxAst1,x,y) or dansBoite(BoxAst2,x,y)) and (not invstate):
+			PointsVie-=1
+			invstate = True
+		elif not (dansBoite(BoxAst1,x,y) or dansBoite(BoxAst2,x,y)):
+			invstate = False
+		if PointsVie==0:
+			jeu=False
+
+		# On update la distance avec la vitesse de la fusée
+		Distance -= ((abs(momentumX)+abs(momentumY))*1000)*0.002
+		if Distance < 10:
+			# TODO : LANCER LA SUITE
+			jeu = False
+
+		# On update les textes
+		txtPV = texte("Etat du fuselage : "+str(PointsVie),30)
+		txtVitesse=texte("Vitesse de la fusée : "+str((int(abs(momentumX)+abs(momentumY)))*1000)+" m/s",30)
+		txtDistance = texte("Distance vers l'objectif : "+str(int(Distance))+" m",30)
+
+		# On affiche nos images 
+		ecran.blit(fond,(0,0))
+		ecran.blit(txtPV,(20,20))
+		ecran.blit(txtVitesse,(20,40))
+		ecran.blit(txtDistance,(20,60))
+		ecran.blit(asteroide,(xast1,yast1))
+		ecran.blit(asteroide2,(xast2,yast2))
+		if abs(momentumX)>0 or abs(momentumY)>0:
+			ecran.blit(fuséeflamme, (x,y))
+		else:
+			ecran.blit(fusée, (x,y)) 
+
 		time.sleep(0.002)
 		pygame.display.flip()
 
 
-
-
-		
-		
-
-
 """
-while jeu:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			jeu = False
-		if event.type == KEYDOWN:
-			if event.key == K_ESCAPE:
-				jeu = False
-			if event.key == K_z:
-				print("Z")
-			if event.key == K_s:
-				print("S")
-			if event.key == K_d:
-				print("D")
-			if event.key == K_q:
-				print("Q")
-
-	# ecran.blit(fond,(0,0))
-	# ecran.blit(ballon, (x,y)) # perso
-	time.sleep(0.002)
-	pygame.display.flip()
+------------------------------  LANCEMENT  ------------------------------
 """
 
-jeuEspace()
-
-l , h = select_taille_ecran()
+l , h = select_taille_ecran(1080,500)
 ecran = pygame.display.set_mode((l,h))
 
-
+intro()
 
 
 
